b6285fba3cda8c37672e9ac9fb672a838252e71b
Kevin Sullivan <sullivan.kevinj@gmail.com>
2021-08-19 19:11:36 +0000

    .

--- std/geom3d_std.lean
+++ std/geom3d_std.lean
@@ -31,10 +31,4 @@ http://www.cs.cornell.edu/courses/cs4620/2008fa/asgn/ray1/fcg3-sec245-248.pdf
 (3) ACS is given by [Origin, b0, b1, b2]
 
 -/
-noncomputable def rice440_acs : geom3d_space _ := 
- let origin := mk_position3d geom3d_std_space 0 0 0 in
- let basis0 := mk_displacement3d geom3d_std_space 1 0 0 in
- let basis1 := mk_displacement3d geom3d_std_space 0 1 0 in
- let basis2 := mk_displacement3d geom3d_std_space 0 0 1 in
- let fr := mk_geom3d_frame origin basis0 basis1 basis2 in
-  mk_geom3d_space fr
+
